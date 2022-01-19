65ff9487084bb2563e10fb1d63d9db4ca48a03bf
Ogabek <ogabekabdijabborov@gmail.com>
2022-01-19 11:32:32 +0500

    home

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,13 @@
 import UIKit
 import Flutter
-
+import YandexMapsMobile
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  YMKMapKit.setApiKey("4dcad512-88bd-4983-9d79-b6ae71e05733")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:oson_apteka/src/appTheme/app_theme.dart';
+import 'package:oson_apteka/src/utils/utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,30 +13,75 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    double h = Utils.windowHeight(context);
+    double w = Utils.windowWidth(context);
+    double o = (h + w) / 2;
     return Scaffold(
-      appBar: AppBar(backgroundColor: AppTheme.white,
-        title: Text("Статистика за месяц",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-
-          color: AppTheme.dark32,
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-          fontFamily: AppTheme.fontFamilyGilroy,
-          fontStyle: FontStyle.normal,
+      backgroundColor: AppTheme.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: AppTheme.white,
+        title: Text(
+          "Статистика за месяц",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: AppTheme.dark32,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            fontFamily: AppTheme.fontFamilyGilroy,
+            fontStyle: FontStyle.normal,
+          ),
         ),
+      ),
+      body: Column(children: [
+        Container(
+          height: 64 * h,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.only(left: 16 * w, right: 16 * w, top: 24 * h),
+          padding: EdgeInsets.only(
+            left: 28 * w,
+            right: 28 * w,
+          ),
+          child: Row(
+            children: [
+              Container(
+             color: Colors.transparent,
+                child: SvgPicture.asset(
+                  "assets/icons/arrow_left.svg",
+                ),
+              ),
+              const Spacer(),
 
+              Text(
+                "Март",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppTheme.dark32,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: AppTheme.fontFamilyGilroy,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              const Spacer(),
+              Container(
+                color: Colors.transparent,
+                child: SvgPicture.asset(
+                  "assets/icons/arrow_right.svg",
+                ),
+              ),
 
+            ],
+          ),
         ),
-
-
-
-
-      ),
-      body: Column(
-          children: [
-
-      ),
+        Container(height: 305*h,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.only(left: 16 * w, right: 16 * w, ),
+          child: ClipRRect(
+            child: Image.asset("assets/images/graphic.png",),
+          ),
+        ),
+      ]),
     );
   }
 }
